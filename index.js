620d2723d4fcaf6d2277de1ccc3ceda392caf495
IMG-2017\Stud <stud@ase.ro>
2022-10-04 17:45:51 +0300

    sem2

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const req = require("express/lib/request")
+
+const app = express()
+
+app.get('/ping', (req, res) => {
+    res.send('Pong')
+})
+
+app.listen(8080)
+
+
+var number = 1;
+let bool = true;
+const string = "HELLO";
+
+let obj = {
+    key: "object"
+}
+
+var d = function datatype () {
+    return true;
+}
+
+console.log("Must be number: ", typeof number);
+console.log("Must be boolean: ", typeof bool);
+console.log("Must be string: ", typeof string);
+console.log("Must be object: ", typeof obj);
+console.log("Must be function: ", typeof d);
